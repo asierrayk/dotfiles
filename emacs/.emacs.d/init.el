e953bed4ad3dce40c6a553cb1b0827fa7a876d4d
Asier Cardoso <asier314@gmail.com>
2018-03-15 00:38:19 +0100

    emacs.d

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -5,7 +5,8 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
- '(custom-enabled-themes (quote (tango-dark))))
+ '(custom-enabled-themes (quote (tango-dark)))
+ '(package-selected-packages (quote (auto-virtualenv elpy))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -13,6 +14,33 @@
  ;; If there is more than one, they won't work right.
  )
 
+
+
+;; INSTALL PACKAGES
+;; --------------------------------------
+
+(require 'package)
+(package-initialize)
+
+(elpy-enable)
+
+(add-to-list 'package-archives
+     '("melpa" . "http://melpa.org/packages/") t)
+
 (add-to-list 'load-path "~/.dotfiles/emacs/packages/neotree")
 (require 'neotree)
 (global-set-key [f8] 'neotree-toggle)
+
+
+;; BASIC CUSTOMIZATION
+;; --------------------------------------
+
+(global-linum-mode t) ;; enable line numbers globally
+
+
+;; MAPPINGS CUSTOMIZATION
+;; --------------------------------------
+(global-set-key (kbd "C-?") 'help-command)
+(global-set-key (kbd "M-?") 'mark-paragraph)
+(global-set-key (kbd "C-h") 'delete-backward-char)
+(global-set-key (kbd "M-h") 'backward-kill-word)
